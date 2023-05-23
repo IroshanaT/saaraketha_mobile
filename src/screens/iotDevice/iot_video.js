ebf70a04cd059292932f2a2538987d172d6ed73e
“Upeka123git config --global user.password uC0766771534@git config --global user.email “it19197760@my.sliit.lk <upekapathirana@Upekas-MacBook-Pro.local>
2023-05-24 01:57:29 +0530

    recommendation updated

--- src/screens/iotDevice/iot_video.js
+++ src/screens/iotDevice/iot_video.js
@@ -1,13 +1,12 @@
-// import React from "react";
-// import { View } from "react-native";
-// import { WebView } from "react-native-webview";
-// import { Card, Button } from "react-native-paper";
+import React from "react";
+import { View } from "react-native";
+import { WebView } from "react-native-webview";
+import { Card, Button } from "react-native-paper";
 
-// const VideoComponent = () => {
-//   //http://192.168.181.70:4747/video
-//   const droidCamUrl = "http://192.168.1.10:8000/stream.mjpg";
-//   return <WebView source={{ uri: droidCamUrl }} style={{ flex: 1 }} />;
-// };
+const VideoComponent = () => {
+  //http://192.168.181.70:4747/video
+  const droidCamUrl = "http://192.168.1.10:8000/stream.mjpg";
+  return <WebView source={{ uri: droidCamUrl }} style={{ flex: 1 }} />;
+};
 
-// export { VideoComponent };
-
+export { VideoComponent };
